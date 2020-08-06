c75a2e79159cc228f1068620174fd5b8f5078e09
bellamerinda <68842672+bellamerinda@users.noreply.github.com>
2020-08-06 18:30:01 +0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-import Navbar from '../component/navbar';
-import Layout from '../layouts/layout';
+import Header from '../component/header';
+import Footer from '../component/footer';
 
 function Landingpage() {
     return(
-      <Layout>
-        <Navbar />
-        <div className="container">
-        <img src="images/awal.jpg" width="50" /> 
-        </div>
-      </Layout>
+      <div>
+        <Header/>
+        <center><p className ="title is-2">Chocolate Lava</p>
+            <p className="subtitle is-6">Chocolate lava adalah makanan dessert yang menjamin anda  ingin mencobanya karena lumer dari coklat.</p>
+            <img src="/images/awal.jpg"/>
+            </center>
+        <Footer/>
+      </div>
     );
   }
 
--- pages/makanan.js
+++ pages/makanan.js
@@ -1,82 +1,72 @@
-import Layout from'../layouts/layout';
-import Link from 'next/link';
-function Chocolate (){
-    return(
-        <Layout>
-            <section>
-            <center><p className ="title is-2">Chocolate Lava</p></center>
-            <br/>
-            <p className="subtitle is-6">Chocolate lava adalah makanan dessert yang menjamin anda  ingin mencobanya karena lumer dari coklat.</p>
-            <br />
-            <h6><Link href="/menumakanan" className="font wight-bold active"></Link></h6>
-            <br/>
-            <center>
-                <figure>
-                    <div class="columns is-mobile">
-                    <div class ="column"><p class="bd-notification is-info"><img src="images.baru1.jpg" widht="250"/></p>
-                    </div>
-                    </div>
+import Header from '../component/header'
+import Footer from '../component/footer'
 
+export default function Beranda(){
+    return (
+        <div>
+            <Header/>
+            <center><p class ="title is-2">Chocolate Lava</p>
+            <p class="subtitle is-6">Chocolate lava adalah makanan dessert yang menjamin anda  ingin mencobanya karena lumer dari coklat.</p>
+            <div class="columns is-mobile">
+                    <div class ="column"><p class="bd-notification is-info"><img src="/images/baru1.jpg" width="250"/></p>
+                    </div>
+                    </div>
                     <div class="columns is-mobile">
-                    <div class ="column"><p class="bd-notification is-info"><img src="images.baru2.jpg" widht="250"/></p>
+                    <div class ="column"><p class="bd-notification is-info"><img src="images/baru2.jpg" width="250"/></p>
+                    </div>
                     </div>
 
                    <div class="columns is-mobile">
-                    <div class ="column"><p class="bd-notification is-info"><img src="images.baru3.jpg" widht="250"/></p>
+                    <div class ="column"><p class="bd-notification is-info"><img src="images/baru3.jpg" width="250"/></p>
+                    </div>
                     </div>
 
                     <div class="columns is-mobile">
-                    <div class ="column"><p class="bd-notification is-info"><img src="images.baru4.jpg" widht="250"/></p>
+                    <div class ="column"><p class="bd-notification is-info"><img src="images/baru4.jpg" width="250"/></p>
+                    </div>
                     </div>
 
                     <div class="columns is-mobile">
-                    <div class ="column"><p class="bd-notification is-info"><img src="images.baru5.jpg" widht="250"/></p>
+                    <div class ="column"><p class="bd-notification is-info"><img src="images/baru5.jpg" width="250"/></p>
+                    </div>
                     </div>
 
                     <div class="columns is-mobile">
-                    <div class ="column"><p class="bd-notification is-info"><img src="imagES.baru6.jpg" widht="250"/></p>
+                    <div class ="column"><p class="bd-notification is-info"><img src="images/baru6.jpg" width="250"/></p>
+                    </div>
                     </div>
 
                     <div class="columns is-mobile">
-                    <div class ="column"><p class="bd-notification is-info"><img src="imags.baru7.jpg" widht="250"/></p>
+                    <div class ="column"><p class="bd-notification is-info"><img src="images/baru7.jpg" width="250"/></p>
+                    </div>
                     </div>
 
                     <div class="columns is-mobile">
-                    <div class ="column"><p class="bd-notification is-info"><img src="images.baru8.jpg" widht="250"/></p>
+                    <div class ="column"><p class="bd-notification is-info"><img src="images/baru8.jpg" width="250"/></p>
+                    </div>
                     </div>
 
                     <div class="columns is-mobile">
-                    <div class ="column"><p class="bd-notification is-info"><img src="images.baru9.jpg" widht="250"/></p>
+                    <div class ="column"><p class="bd-notification is-info"><img src="images/baru9.jpg" width="250"/></p>
+                    </div>
                     </div>
 
                     <div class="columns is-mobile">
-                    <div class ="column"><p class="bd-notification is-info"><img src="images.baru10.jpg" widht="250"/></p>
+                    <div class ="column"><p class="bd-notification is-info"><img src="images/baru10.jpg" width="250"/></p>
+                    </div>
                     </div>
 
                     <div class="columns is-mobile">
-                    <div class ="column"><p class="bd-notification is-info"><img src="images.baru11.jpg" widht="250"/></p>
+                    <div class ="column"><p class="bd-notification is-info"><img src="images/baru11.jpg" width="250"/></p>
+                    </div>
                     </div>
 
                     <div class="columns is-mobile">
-                    <div class ="column"><p class="bd-notification is-info"><img src="images.feed.jpg.jpg" widht="250"/></p>
-                    </div>
-
+                    <div class ="column"><p class="bd-notification is-info"><img src="images/feed.jpg" width="250"/></p>
                     </div>
                     </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    </figure>
-                </center>
-        </section>
-        </Layout>
-    );
+            </center>
+            <Footer/>
+        </div>
+    )
 }
-
-export default Chocolate;
